3400783450743074a19aea86076679ac1c22a028
Aditi Umashankar <aditiu1209@gmail.com>
2024-02-13 17:46:51 +0530

    Used fetched data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Select from 'react-select'
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 
@@ -9,11 +9,6 @@ function App() {
   const searchParams = new URLSearchParams(document.location.search);
   const [users, setUsers] = useState([]);
 
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]; 
   useEffect(() => {
     fetch('https://connections-societal-thinking-f49fc4a0c28b.herokuapp.com/users')
       .then((res) => {
@@ -21,30 +16,33 @@ function App() {
       })
       .then((data) => {
         console.log(data);
-        setUsers(data);
+        const mappedData = data.map((value) => {
+          return { value: value._id, label: value.name + ' - ' + value.organisation }
+        });
+        setUsers(mappedData);
       });
   }, []);
   console.log(searchParams);
   return (
     <div className="App">
-      {searchParams.get('user') ? 
-  <Select options={options} />
-: <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to           {searchParams.get('user')} reload.
+      {searchParams.get('user') ?
+        <Select options={users} />
+        : <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to           {searchParams.get('user')} reload.
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>}
-      
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>}
+
     </div>
   );
 }
