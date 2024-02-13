16642f549f092af5bb0edc8a215318cd60f39b3d
Aditi Umashankar <aditiu1209@gmail.com>
2024-02-13 17:26:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select'
+import React, {useState,useEffect} from 'react'
+
+
 
 function App() {
+  const searchParams = new URLSearchParams(document.location.search);
+  const [users, setUsers] = useState([]);
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]; 
+  useEffect(() => {
+    fetch('https://connections-societal-thinking-f49fc4a0c28b.herokuapp.com/users')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+  console.log(searchParams);
   return (
     <div className="App">
-      <header className="App-header">
+      {searchParams.get('user') ? 
+  <Select options={options} />
+: <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to           {searchParams.get('user')} reload.
+
         </p>
         <a
           className="App-link"
@@ -17,7 +43,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>}
+      
     </div>
   );
 }
